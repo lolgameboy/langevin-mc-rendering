dd14f67ab4edca7b90ee8694ed0a34df98595e0d
Wannes Croes <wannes.croes@wina.be>
2025-12-12 11:48:08 +0100

    Fucking Bingo!!! Main loop jit-traced, 100.000x speedup

--- pss_integrator.py
+++ pss_integrator.py
@@ -8,6 +8,7 @@ class Pss(mi.Integrator):
         pass
     
     @dr.syntax
+    #@dr.freeze
     def render(self, scene: mi.Scene, sensor: mi.Sensor, seed: mi.UInt = 0, spp: int = 0, develop: bool = True, evaluate: bool = True) -> mi.TensorXf:
         film = sensor.film()
         resolution = film.crop_size()
@@ -19,7 +20,7 @@ class Pss(mi.Integrator):
         )
 
         cam_transform = sensor.m_to_world
-        rng = dr.rng(0)
+        rng = dr.rng(seed=mi.UInt32(0))
 
         # Instantiate a path tracer object
         pathtracer = mi.load_dict({
@@ -37,12 +38,13 @@ class Pss(mi.Integrator):
         # Initial sample
         sample = dr.full(mi.ArrayXf, 0.5, (sample_size,1)) # [0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5]
         print(sample)
-        luminance, res, pixel_x, pixel_y = self.calculate_sample_contribution(sample, scene, pathtracer, cam_transform, plane_size, resolution)
+        luminance, res, pixel_x, pixel_y = calculate_sample_contribution(sample, scene, pathtracer, cam_transform, plane_size, resolution)
 
         image_block.put(mi.Point2f(pixel_x, pixel_y), res[0] / luminance)
 
-        N = 10000
-        for i in range(N):
+        N = mi.Int(10000000)
+        i = mi.Int(0)
+        while i < N:
             # Generate proposal mutation
             mutation = rng.normal(mi.ArrayXf, (sample_size,1)) * 0.1
             # mutation = [0.05,0.05,0.05,0.05,0.05,0.05,0.05,0.05,0.05,0.05,0.05,0.05,0.05,0.05,0.05]
@@ -52,11 +54,11 @@ class Pss(mi.Integrator):
             prop_sample = sample + mutation
 
             # Boundary conditions: loop around
-            for i in range(len(prop_sample)):
-                if prop_sample[i] < 0:
-                    prop_sample[i] += 1
-                if prop_sample[i] > 1:
-                    prop_sample[i] -= 1
+            for j in range(len(prop_sample)):
+                if prop_sample[j] < 0:
+                    prop_sample[j] += 1
+                if prop_sample[j] > 1:
+                    prop_sample[j] -= 1
 
             # TODO make it more efficient (if it is worth it)
             # if prop_sample < 0:
@@ -65,19 +67,17 @@ class Pss(mi.Integrator):
             #    prop_sample -= 1
 
             # Calculate proposal luminance
-            prop_luminance, prop_res, prop_pixel_x, prop_pixel_y = self.calculate_sample_contribution(prop_sample, scene, pathtracer, cam_transform, plane_size, resolution)
+            prop_luminance, prop_res, prop_pixel_x, prop_pixel_y = calculate_sample_contribution(prop_sample, scene, pathtracer, cam_transform, plane_size, resolution)
             
             # Calculate acceptence chance            
             a = dr.min(1, prop_luminance / luminance)
 
-            print(a)
-            # print(rng.uniform(mi.Float, 1)[0])
+            # print(rng.uniform(mi.Float, 1))
             # TODO Is dit correct?
-            if rng.uniform(mi.Float, 1)[0] < a:
-               luminance, res, pixel_x, pixel_y = prop_luminance, prop_res, prop_pixel_x, prop_pixel_y
-               sample = prop_sample
+            if rng.uniform(mi.Float, 1) < a:
+                luminance, res, pixel_x, pixel_y = prop_luminance, prop_res, prop_pixel_x, prop_pixel_y
+                sample = prop_sample
 
-            print(sample)
             # Explanation: PSSMLT actually uses luminance (combination of RGB values) to explore domain 
             # (because it only takes a scalar output) to mutate the sample)
             # Then it intuitively counts pixel visits to determine luminance distribution over the whole image
@@ -86,6 +86,8 @@ class Pss(mi.Integrator):
             # print("RES")
             # print(res[0] / luminance)
             image_block.put(mi.Point2f(pixel_x, pixel_y), res[0] / luminance)
+            i += 1
+            pass
         
         # Samples have now simply counted all samples per bucket. 
         # Now we must appropriately devide the whole image to get a proper distribution that integrates to 1
@@ -95,26 +97,27 @@ class Pss(mi.Integrator):
         print(image_block.tensor() / N * resolution.x * resolution.y / 3)
         return image_block.tensor() / N * resolution.x * resolution.y / 3
     
-    def calculate_sample_contribution(self, sample, scene, pathtracer, cam_transform, plane_size, resolution):
-        pss_sampler = PssSampler(sample)
+#@dr.freeze
+def calculate_sample_contribution(sample, scene, pathtracer, cam_transform, plane_size, resolution):
+    pss_sampler = PssSampler(sample)
 
-        # Sample an initial ray through the image plane
-        ray_origin_local = mi.Vector3f(0, 0, 0)
-        rand_x = pss_sampler.next_1d()
-        rand_y = pss_sampler.next_1d()
-        x = -plane_size.x / 2 + rand_x * plane_size.x
-        y = -plane_size.y / 2 + rand_y * plane_size.y
-        ray_direction_local = dr.normalize(mi.Vector3f(x, y, 1))
+    # Sample an initial ray through the image plane
+    ray_origin_local = mi.Vector3f(0, 0, 0)
+    rand_x = pss_sampler.next_1d()
+    rand_y = pss_sampler.next_1d()
+    x = -plane_size.x / 2 + rand_x * plane_size.x
+    y = -plane_size.y / 2 + rand_y * plane_size.y
+    ray_direction_local = dr.normalize(mi.Vector3f(x, y, 1))
 
-        # Also determine affected pixel already
-        pixel_x = mi.Float(rand_x * resolution.x)
-        pixel_y = mi.Float(rand_y * resolution.y)
+    # Also determine affected pixel already
+    pixel_x = mi.Float(rand_x * resolution.x)
+    pixel_y = mi.Float(rand_y * resolution.y)
 
-        ray = mi.Ray3f(o=cam_transform.translation() + ray_origin_local, d=cam_transform.transform_affine(ray_direction_local))
-        diffray = mi.RayDifferential3f(ray)
+    ray = mi.Ray3f(o=cam_transform.translation() + ray_origin_local, d=cam_transform.transform_affine(ray_direction_local))
+    diffray = mi.RayDifferential3f(ray)
 
-        # Sample this ray
-        res = pathtracer.sample(scene, pss_sampler, diffray)
-        luminance = dr.sum(res[0]) / 3 # TODO This is where luminance weights come in, now I use equal weights
+    # Sample this ray
+    res = pathtracer.sample(scene, pss_sampler, diffray)
+    luminance = dr.sum(res[0]) / 3 # TODO This is where luminance weights come in, now I use equal weights
 
-        return luminance, res, pixel_x, pixel_y
+    return luminance, res, pixel_x, pixel_y
